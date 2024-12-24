1b3d1a98ed09feba4fe2f7ea131442c15672fbe3
Isaac Levy <isaaclevy004@gmail.com>
2024-12-24 15:16:32 +0200

    Change ball background each click

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ function onBallClick() {
     if (gSize < 400) gSize += getRandomInt(20, 61)
     else gSize = 100
 
+    elBall.style.backgroundColor = getRandomColor()
     elBall.style.width = gSize + 'px'
     elBall.style.height = gSize + 'px'
     elBall.innerText = gSize
