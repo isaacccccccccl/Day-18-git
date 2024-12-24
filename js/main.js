6acc9488da7b4eaef287ad96bdbe0225bb404e0c
Isaac Levy <isaaclevy004@gmail.com>
2024-12-24 14:14:18 +0200

    Change on click function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 'use strict'
 
+var gSize = 100
+
 function onBallClick() {
-    console.log('ball clicked')
+    var elBall = document.querySelector('.ball')
+    gSize += 50
+    elBall.style.width = gSize + 'px'
+    elBall.style.height = gSize + 'px'
+    elBall.innerText = gSize
 }
