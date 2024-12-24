97c4e1ae140928568a6b5dfed5a3306635244f0e
Isaac Levy <isaaclevy004@gmail.com>
2024-12-24 16:25:11 +0200

    Add onClickFourth function

--- js/main.js
+++ js/main.js
@@ -25,3 +25,21 @@ function onBallThreeClick() {
     }
 
 }
+
+function onBallFourthClick() {
+
+    var elBalls = document.querySelectorAll('.ball')
+
+    for (var i = 0; i < elBalls.length; i++) {
+        var elBall = document.querySelector(`.ball${i+1}`)
+
+        gSize = +elBall.innerText - getRandomInt(20, 61)
+    if (gSize < 100) gSize = 100
+
+
+    elBall.style.backgroundColor = getRandomColor()
+    elBall.style.width = gSize + 'px'
+    elBall.style.height = gSize + 'px'
+    elBall.innerText = gSize
+    } 
+}
