f70dc7b0b5940abc618b568a578ed8a468b779ba
Isaac Levy <isaaclevy004@gmail.com>
2024-12-24 16:00:31 +0200

    Add thierd ball with new onclick

--- js/main.js
+++ js/main.js
@@ -7,10 +7,21 @@ function onBallClick(onBtn, maxDiameter) {
     var elBall = onBtn
     gSize = +elBall.innerText + getRandomInt(20, 61)
     if (gSize > maxDiameter) gSize = 100
-    
+
 
     elBall.style.backgroundColor = getRandomColor()
     elBall.style.width = gSize + 'px'
     elBall.style.height = gSize + 'px'
     elBall.innerText = gSize
+}
+
+function onBallThreeClick() {
+
+    var elBalls = document.querySelectorAll('.ball')
+
+    for (var i = 0; i < elBalls.length; i++) {
+        var elBall = document.querySelector(`.ball${i+1}`)
+        onBallClick(elBall, 400)
+    }
+
 }
